080397b0c8940ac29ee0a049fce25e906838b7ca
icyleaf <icyleaf.cn@gmail.com>
2017-05-05 18:27:14 +0800

    maybe found crystal bug, removed result temporary

--- code/array/frist-vs-index[0].cr
+++ code/array/frist-vs-index[0].cr
@@ -3,14 +3,14 @@ require "benchmark"
 ARRAY = (1..100).to_a
 
 def fast
-  ARRAY[0]
-end
-
-def slow
   ARRAY.first
 end
 
+def slow
+  ARRAY[0]
+end
+
 Benchmark.ips do |x|
-  x.report("Array#[0]") { fast }
-  x.report("Array#first") { slow }
+  x.report("Array#first") { fast }
+  x.report("Array#[0]") { slow }
 end
