97b10c277dee5654dc4646a87bdd5e093cdafd87
RX14 <chris@rx14.co.uk>
2017-05-09 18:41:26 +0100

    Add tuple versions of enumerable flatten benchmarks

--- code/enumerable/map-flatten-vs-flat_map.cr
+++ code/enumerable/map-flatten-vs-flat_map.cr
@@ -2,15 +2,25 @@ require "benchmark"
 
 ARRAY = (1..100).to_a
 
+def fastest
+  ARRAY.flat_map { |e| {e, e} }
+end
+
 def fast
-  ARRAY.flat_map { |e| [e, e] }
+  ARRAY.map { |e| {e, e} }.flatten
 end
 
 def slow
+  ARRAY.flat_map { |e| [e, e] }
+end
+
+def slowest
   ARRAY.map { |e| [e, e] }.flatten
 end
 
 Benchmark.ips do |x|
-  x.report("Array#flat_map") { fast }
-  x.report("Array#map.flatten") { slow }
+  x.report("Array#flat_map (Tuple)") { fastest }
+  x.report("Array#map.flatten (Tuple)") { fast }
+  x.report("Array#flat_map (Array)") { slow }
+  x.report("Array#map.flatten (Array)") { slowest }
 end
